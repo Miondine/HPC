4c984aa182fc8ae642e21ce327ac6452e5353af1
HOFMANN <skcn96@hamilton1.hpc.dur.ac.uk>
2020-11-24 15:42:03 +0000

    implemented ring_reduce function

--- code/mpi/ring/ring.c
+++ code/mpi/ring/ring.c
@@ -17,7 +17,18 @@ static void ring_reduce(const int *sendbuf, int *recvbuf, MPI_Comm comm)
    *
    * (x - y + N) % N;
    */
+  int rank;
+  int size;
+  MPI_Comm_rank(comm, &rank);
+  MPI_Comm_size(comm, &size);
+  int right_rank = (rank + 1) % size;
+  int left_rank = (rank - 1 + size) % size;
+  int MPI_Sendrecv(*sendbuf,1,MPI_INT,right_rank,0,*recvbuf,
+                  1,MPI_INT,left_rank,0,comm,MPI_STATUS_IGNORE);
+
+  return;
 }
+
 
 int main(int argc, char **argv)
 {
@@ -33,11 +44,11 @@ int main(int argc, char **argv)
   int local_value = rank;
 
   int summed_value = 0;
-
+  for (int i = 0; i < size; i++) {
   printf("[%d] Before reduction: local value is %d; summed value is %d\n",
          rank, local_value, summed_value);
   ring_reduce(&local_value, &summed_value, comm);
-
+  }
   printf("[%d] After reduction: local value is %d; summed value is %d\n",
          rank, local_value, summed_value);
   MPI_Finalize();
